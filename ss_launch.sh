5abe50defcad00c638d9620539054c990036835c
Ubuntu <ubuntu@ip-10-0-1-47.ap-southeast-2.compute.internal>
2017-02-16 15:31:38 +0000

    ss launch

---
+++ ss_launch.sh
@@ -0,0 +1,3 @@
+./rsc ss launch /catalog/catalogs/1000/applications/58a5563ed634e6000d018eaf 'name=jt_test'
+
+Supported flags are: description, end_date, name, options[][name], options[][type], options[][value], schedule_name
