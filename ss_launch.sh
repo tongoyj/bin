a4f157bdb09b69fa9187627b1383d8f270b61cf7
Ubuntu <ubuntu@ip-10-0-1-47.ap-southeast-2.compute.internal>
2017-02-16 15:46:27 +0000

    ss launch

--- ss_launch.sh
+++ ss_launch.sh
@@ -1,3 +1,3 @@
-./rsc ss launch /catalog/catalogs/1000/applications/58a5563ed634e6000d018eaf 'name=jt_test'
+./rsc ss launch /catalog/catalogs/1000/applications/58a5563ed634e6000d018eaf 'name=jt_test' 'scedule_name=JT Weekday'
 
 Supported flags are: description, end_date, name, options[][name], options[][type], options[][value], schedule_name
